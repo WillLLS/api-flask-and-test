9c4e0afc3c903d3c9334ef6e7a1ade7d23e9b825
William <lalis.william@gmail.com>
2022-11-15 01:58:32 +0100

    modify original db for reset

--- jeu_de_test/test.sh
+++ jeu_de_test/test.sh
@@ -1,8 +1,8 @@
 # This two lines reset the database
 rm ./../Service_Web/video1.db
-cp ./../video1.db ./../Service_Web/video1.db
+cp ./../DB_origin.db ./../Service_Web/video1.db
 
 python3 ./lib_test/main.py
 
 rm ./../Service_Web/video1.db
-cp ./../video1.db ./../Service_Web/video1.db
+cp ./../DB_origin.db ./../Service_Web/video1.db
